f963e26a7ab814d5a9d2281d434c4fbd54405c7a
jhkim5659 <wjdgus5659@naver.com>
2020-01-19 15:30:45 +0900

    SpringBootApplication 파일 주석 추가

--- src/main/java/com/peer/peerssv2/Peerssv2Application.java
+++ src/main/java/com/peer/peerssv2/Peerssv2Application.java
@@ -3,6 +3,13 @@ package com.peer.peerssv2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot App 실행 클래스
+ * 
+ * @class Peerssv2Application.java
+ * @author jhkim
+ * @since 2020-01-19
+ */
 @SpringBootApplication
 public class Peerssv2Application {
 
